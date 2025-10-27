f44eeb02057bb989eae36229dddd6343958bc593
Rezwan66 <shaikhrezwan66@gmail.com>
2025-10-27 22:19:31 +0100

    added delete parcel api

--- index.js
+++ index.js
@@ -1,3 +1,101 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjzxbzp.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db('pakjetDB');
+    const parcelCollection = db.collection('parcels');
+
+    // GET: get all parcels
+    // app.get('/parcels', async (req, res) => {
+    //   const parcels = await parcelCollection.find().toArray();
+    //   res.send(parcels);
+    // });
+    //GET: a percel
+    app.get('/parcels', async (req, res) => {
+      try {
+        const userEmail = req.query.email;
+        const query = userEmail ? { created_by: userEmail } : {};
+        const options = {
+          sort: { creation_date: -1 }, // newest first
+        };
+        const parcels = await parcelCollection.find(query, options).toArray();
+        res.send(parcels);
+      } catch (error) {
+        console.error('Error fetching parcels: ', error);
+        res.status(500).send({ message: 'Failed to get parcel' });
+      }
+    });
+    // POST: create a parcel
+    app.post('/parcels', async (req, res) => {
+      try {
+        const newParcel = req.body;
+        const result = await parcelCollection.insertOne(newParcel);
+        res.status(201).send(result);
+      } catch (error) {
+        console.error('Error inserting parcel: ', error);
+        res.status(500).send({ message: 'Failed to create parcel' });
+      }
+    });
+    // DELETE: a parcel
+    app.delete('/parcels/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const result = await parcelCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        //    if(result.deletedCount===0){
+        //     return res.status(404).send({message: 'Parcel not found'})
+        //    }
+        res.send(result);
+      } catch (error) {
+        console.log('Error deleting parcel:', error);
+        res.status(500).send({ message: 'Failed to delete parcel' });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('PakJet Server is running 🚀');
+});
+
+// Start server
+app.listen(port, () => console.log(`✅ Server listening on port ${port}`));
